8843ab3a7386950a377cc873018e325887ba3257
Carter <deseancarter1990@gmail.com>
2021-04-10 01:11:59 -0400

    building functionality for sorting employees

--- employee-directory/src/pages/Search.js
+++ employee-directory/src/pages/Search.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Card from '../components/Card';
 import SearchForm from '../components/SearchForm';
-//import Wrapper from './components/Wrapper';
-//import Col from './components/Col';
 import API from '../utils/API';
-//import './App.css';
 import '../styles/Table.css';
 
 class Search extends React.Component {
